9c1463585c2409575a28223736c905f136d23b39
Rikka <rikka@shizuku.moe>
2018-01-11 19:42:17 +0800

    feat: update libraries

--- app/src/main/java/moe/shizuku/fcmformojo/BaseActivity.java
+++ app/src/main/java/moe/shizuku/fcmformojo/BaseActivity.java
@@ -1,9 +1,12 @@
 package moe.shizuku.fcmformojo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
+
+import java.io.File;
 
 import moe.shizuku.fontprovider.FontProviderClient;
 import moe.shizuku.fontprovider.FontRequest;
@@ -15,23 +18,36 @@ import moe.shizuku.fontprovider.FontRequests;
 
 public class BaseActivity extends FragmentActivity {
 
-    private static boolean sFontProviderInitialized = false;
+    private static boolean sFontInitialized = false;
+
+    private static void initializeFont(Context context) {
+        if (sFontInitialized) {
+            return;
+        }
+        sFontInitialized = true;
+
+        try {
+            if (!new File("/system/fonts/NotoSansCJK-Medium.ttc").exists()
+                    && !new File("/system/fonts/NotoSansCJKsc-Medium.ttf").exists()
+                    && !new File("/system/fonts/NotoSansCJKsc-Medium.otf").exists()) {
+                FontRequests.DEFAULT_SERIF_FONTS = new FontRequest[]{FontRequest.DEFAULT, FontRequest.NOTO_COLOR_EMOJI};
+
+                FontProviderClient client = FontProviderClient.create(context);
+                if (client != null) {
+                    client.replace(FontRequests.DEFAULT_SERIF_FONTS, "Noto Sans CJK",
+                            "sans-serif", "sans-serif-medium");
+                }
+            }
+        } catch (Throwable tr) {
+            tr.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        initializeFont(this);
+
         super.onCreate(savedInstanceState);
-
-        if (!sFontProviderInitialized) {
-            FontRequests.DEFAULT_SERIF_FONTS = new FontRequest[]{FontRequest.DEFAULT, FontRequest.NOTO_COLOR_EMOJI};
-
-            FontProviderClient client = FontProviderClient.create(this);
-            if (client != null) {
-                client.replace(FontRequests.DEFAULT_SERIF_FONTS, "Noto Sans CJK",
-                        "sans-serif", "sans-serif-medium");
-            }
-
-            sFontProviderInitialized = true;
-        }
     }
 
     @Override
